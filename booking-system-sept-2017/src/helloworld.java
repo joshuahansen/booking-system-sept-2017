7d846adb015f3d1a370c0da78d3dc199ca021b9e
zwing <zwing@Ramach-Surface>
2017-03-09 17:05:46 +1100

    Test commit from Zach to master branch.
    Change-Id: I648f667eae8c0de2d930dab21388fae6bc6e8f8b
    Signed-off-by: zwing <zwing@Ramach-Surface>

--- booking-system-sept-2017/src/helloworld.java
+++ booking-system-sept-2017/src/helloworld.java
@@ -1,11 +1,8 @@
-
-public class helloworld {
-
-	public static void main(String[] args) {
-		
-		//Simple test to get github working with multiple computers using the hello world program 
+public class helloworld
+{
+	public static void main(String[] args)
+	{
 		System.out.println("Hello World, from Josh.");
 		System.out.println("Hello World, from Zach.");
 	}
-
 }
