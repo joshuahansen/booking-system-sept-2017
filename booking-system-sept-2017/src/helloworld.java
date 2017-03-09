2e3d130533ad0f05ea872b8b56aeef22e16e5a58
zwing <zwing@Ramach-Surface>
2017-03-09 16:53:43 +1100

    Initial commit from Zach.
    Change-Id: Iebe1dd6c30a874f5cced02899fcc5eded757d52b
    Signed-off-by: zwing <zwing@Ramach-Surface>

--- booking-system-sept-2017/src/helloworld.java
+++ booking-system-sept-2017/src/helloworld.java
@@ -4,7 +4,8 @@ public class helloworld {
 	public static void main(String[] args) {
 		
 		//Simple test to get github working with multiple computers using the hello world program 
-		System.out.println("Hello World");
+		System.out.println("Hello World, from Josh.");
+		System.out.println("Hello World, from Zach.");
 	}
 
 }
