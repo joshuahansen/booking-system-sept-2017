ab42ce76e8c80a46e3a3f2989af2e9e2834e2bd5
rmit-s3589185-joshua-hansen <s3589185@student.rmit.edu.au>
2017-03-15 11:48:35 +1100

    test commit

--- booking-system-sept-2017/src/helloworld.java
+++ booking-system-sept-2017/src/helloworld.java
@@ -5,6 +5,6 @@ public class helloworld
 		System.out.println("Hello World, from Josh.");
 		System.out.println("Hello World, from Zach. Commit test 5 billion.");
 		System.out.println("Hello world, from Lohgan. This is a test");
-		System.out.println("Hello world, from Lohgan. This is a test from the test branch to test contributions");
+		System.out.println("Hello world, from Lohgan. This is a test from the test.");
 	}
 }
