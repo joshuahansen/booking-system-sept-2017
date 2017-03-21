6f9f4dc3587bd2b20faa6afd6e113d76b433f840
rmit-s3601235-lohgan-nash <s3601235@student.rmit.edu.au>
2017-03-21 20:39:58 +1100

    More validation and fixing bugs
    Change-Id: I2125cb4f8fe38eec530ecd078d9994465a253803
    Signed-off-by: rmit-s3601235-lohgan-nash <s3601235@student.rmit.edu.au>

--- booking-system-sept-2017/src/Registration.java
+++ booking-system-sept-2017/src/Registration.java
@@ -48,22 +48,22 @@ public class Registration {
 		
 		System.out.print("Enter First Name: ");
 		//prompts the user to enter their first name and assigns it to the first name variable
-		this.firstName = keyboard.next();
+		this.firstName = keyboard.nextLine();
 		//prompts the user to enter their last name and assigns it to the last name variable
 		System.out.print("Enter Last Name: ");
-		this.lastName = keyboard.next();
+		this.lastName = keyboard.nextLine();
 		//prompts the user to enter their username and assigns it to the username variable
 		System.out.print("Enter Address: ");
-		this.address = keyboard.next();
+		this.address = keyboard.nextLine();
 		//prompts the user to enter their password and assigns it to the password variable
 		System.out.print("Enter Phone Number: ");
-		this.phone = keyboard.next();
+		this.phone = keyboard.nextLine();
 		//prompts the user to enter their username and assigns it to the username variable
 		System.out.print("Enter Username: ");
-		this.username = keyboard.next();
+		this.username = keyboard.nextLine();
 		//prompts the user to enter their password and assigns it to the password variable
 		System.out.print("Enter Password: ");
-		this.password = keyboard.next();	
+		this.password = keyboard.nextLine();
 	}
 	
 	public boolean registerNewCust(ArrayList<Customer> cust, ArrayList<Business> busi) {
@@ -85,22 +85,33 @@ public class Registration {
 		/*This runs through the customer and the business array list and compares the username of each index to the username 
 		 * entered by the user, and if it matches with an already existing username, it will return false
 		 */		
-		for(int i = 0; i < cust.size(); i++) {
-			
-			if(username.equals(cust.get(i).getUsername())) {
-								
+		for(int i = 0; i < cust.size(); i++) {	
+				if(username.equals(cust.get(i).getUsername())) {					
+					System.out.println("\nUsername already exists!");
+					return false;
+				}
+		}
+		
+		for(int i = 0; i < busi.size(); i++) {	
+			if(username.equals(busi.get(i).getUsername())) {			
 				System.out.println("\nUsername already exists!");
 				return false;
 			}
 		}
-		
-		for(int i = 0; i < busi.size(); i++) {
-			
-			if(username.equals(busi.get(i).getUsername())) {
-				
-				System.out.println("\nUsername already exists!");
-				return false;
-			}
+		//Validates the username length
+		if(username.length() < 3) {
+			System.out.println("\nUsername length must be at least 4 characters!");
+			return false;
+		}
+		//Validates the password length
+		if(password.length() < 5) {
+			System.out.println("\nPassword length must be at least 6 characters!");
+			return false;
+		}
+		//Validates the phone number length
+		if(phone.length() < 7) {
+			System.out.println("\nA valid phone number must contain at least 8 digits!");
+			return false;
 		}
 		
 		return true;
