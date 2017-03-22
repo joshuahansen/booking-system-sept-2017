4ec66978ec6c9b4c8d08d1e1fe70fec6ac1aba8c
rmit-s3539788-zachary-wingrave <s3539788@student.rmit.edu.au>
2017-03-22 11:35:47 +1100

    Acceptance testing for Login function.
    Change-Id: Ied6312d843225df55ee5c133db94b175eae34789
    Signed-off-by: rmit-s3539788-zachary-wingrave <s3539788@student.rmit.edu.au>

--- booking-system-sept-2017/src/users/InitUsers.java
+++ booking-system-sept-2017/src/users/InitUsers.java
@@ -4,25 +4,29 @@ import java.util.ArrayList;
 
 public class InitUsers {
 	//pass in Array List of type customer and assign new customer objects to array
-	public void init_customers(ArrayList<Customer> customers)
+	public boolean init_customers(ArrayList<Customer> customers)
 	{
 		Customer c1 = new Customer("Bob", "Marley", "1 High Street Melbourne", "97342356",
 										"c_bMarley", "bMarley");
 		Customer c2 = new Customer("Vicki", "Vale", "23 Batman Street Melbourne", "34232687", 
-									"VickiV", "VickiV");
+									"c_VickiV", "VickiV");
 		Customer c3 = new Customer("John", "Doe", "6 Cemetery Drive Melbourne", "97346756", 
-									"jd666", "jd666");
+									"c_jd666", "jd666");
 		
 		customers.add(c1);
 		customers.add(c2);
 		customers.add(c3);
+		
+		return true;
 	}
 	//pass in Array List of type business and assign business objects to array
-	public void init_businesses(ArrayList<Business> businesses)
+	public boolean init_businesses(ArrayList<Business> businesses)
 	{
 		Business b1 = new Business("St. George’s Hospital", "Henry", "Gray", "Blackshaw Road Melbourne", 
-									"86721255", "StGeorges", "StGeorges");
+									"86721255", "b_StGeorges", "StGeorges");
 		
 		businesses.add(b1);
+		
+		return true;
 	}
 }
