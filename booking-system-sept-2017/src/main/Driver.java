c498fdb0c9c9b89263e619a26cba1a899f40c3d2
rmit-s3589185-joshua-hansen <s3589185@student.rmit.edu.au>
2017-03-27 22:38:34 +1100

    driver commit

--- booking-system-sept-2017/src/main/Driver.java
+++ booking-system-sept-2017/src/main/Driver.java
@@ -19,6 +19,10 @@ public class Driver {
 		Database database = new Database();
 		
 		Connection connection = database.connectDatabase();
+//		while(connection == null)
+//		{
+//			connection = database.connectDatabase();
+//		}
 		if(database.readCustDB(connection, customers) == true)
 		{
 			System.out.println("Customer Database loaded");
