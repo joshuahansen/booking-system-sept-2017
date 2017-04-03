a9df2a712a15278ea4ec146b07d476b136b46c8d
rmit-s3539788-zachary-wingrave <s3539788@student.rmit.edu.au>
2017-04-03 13:13:17 +1000

    Redesigned menu function. Lohgan to fix RegistrationTest and Tony to fill in // view availabilities code linked with Josh's database.
    Change-Id: Id5615ebab181aef82379823dd8ec19cbe77b23c5
    Signed-off-by: rmit-s3539788-zachary-wingrave <s3539788@student.rmit.edu.au>

--- booking-system-sept-2017/src/main/Menu.java
+++ booking-system-sept-2017/src/main/Menu.java
@@ -1,168 +1,197 @@
 package main;
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 import users.*;
 
 public class Menu 
 {
-	public Menu() {
-		// TODO Auto-generated constructor stub
+	public Menu()
+	{
+		// default constructor
 	}
 	
-	private void menuDisplay() {
-		System.out.println("\nWelcome to the Booking System.\n");
+	public void menuDriver(Scanner userInput, Login login, Registration registration, 
+			ArrayList<Customer> customers, ArrayList<Business> businesses, ArrayList<Employee> employees)
+	{
+		int menuInput = 0;
+		int returnValue = 0;
+		
+		while (menuInput != 3)
+		{
+			printMainMenu();
+			
+			menuInput = userInput.nextInt();
+			
+			switch(menuInput)
+			{
+				case 1:
+				{
+					login.getUsernamePassword(userInput);
+					returnValue = login.login(customers, businesses);
+					
+					if (returnValue == 0)
+					{
+						System.out.println("\nLogin failed: Incorrect username or password.");
+						login.setUsernamePassword(null, null);
+					}
+					else if (returnValue == 1)
+					{
+						System.out.println("\nLogin successful: Printing customer menu.");
+						customerMenu(userInput, registration, customers, businesses, employees);
+					}
+					else if (returnValue == 2)
+					{
+						System.out.println("\nLogin successful: Printing business menu.");
+						businessMenu(userInput, registration, customers, businesses, employees);
+					}
+					break;
+				}
+				case 2:
+				{
+					registration.getUserValues(userInput, customers, businesses);
+					
+					registration.registerNewCust(customers, businesses);
+					break;
+				}
+				case 3:
+				{
+					System.out.println("\nSystem exiting.\n");
+					break;
+				}
+				default:
+				{
+					System.out.println("\nInvalid user input. Please try again.");
+					break;
+				}
+			}
+		}
+	}
+	
+	public void customerMenu(Scanner userInput, Registration registration,  
+			ArrayList<Customer> customers, ArrayList<Business> businesses, ArrayList<Employee> employees)
+	{
+		int menuInput = 0;
+		
+		while (menuInput != 2)
+		{
+			printCustomerMenu();
+			
+			menuInput = userInput.nextInt();
+			
+			switch(menuInput)
+			{
+				case 1:
+				{
+					printAvailabilities(userInput, customers, businesses, employees);
+					break;
+				}
+				case 2:
+				{
+					System.out.println("\nReturning to main menu.\n");
+					break;
+				}
+				default:
+				{
+					System.out.println("\nInvalid user input. Please try again.");
+					break;
+				}
+			}
+		}
+	}
+	
+	public void businessMenu(Scanner userInput, Registration registration, 
+			ArrayList<Customer> customers, ArrayList<Business> businesses, ArrayList<Employee> employees)
+	{
+		int menuInput = 0;
+		
+		while (menuInput != 3)
+		{
+			printBusinessMenu();
+			
+			menuInput = userInput.nextInt();
+			
+			switch(menuInput)
+			{
+				case 1:
+				{
+					printAvailabilities(userInput, customers, businesses, employees);
+					break;
+				}
+				case 2:
+				{
+					registration.addNewEmployee(userInput, employees);
+					break;
+				}
+				case 3:
+				{
+					System.out.println("\nReturning to main menu.\n");
+					break;
+				}
+				default:
+				{
+					System.out.println("\nInvalid user input. Please try again.");
+					break;
+				}
+			}
+		}
+	}
+	
+	public void printMainMenu() 
+	{
+		printMenuLineDouble(30);
+		System.out.println("Welcome to the Booking System.");
+		printMenuLineDouble(30);
 		System.out.println("1. Login");
 		System.out.println("2. Register");
+		System.out.println("3. Exit");
 		System.out.print("\nPlease enter your choice: ");
 	}
 	
-	private void printCustomerMenu()
+	public void printCustomerMenu()
 	{
 		printMenuLineSingle(15);
-		System.out.println("");
 		System.out.println("Customer Menu");
 		printMenuLineSingle(15);
-		System.out.println("");
 		System.out.println("1. View Booking Timetable");
 		System.out.println("2. Logout");
+		System.out.print("\nPlease enter your choice: ");
 	}
 	
-	private void printBusinessMenu()
+	public void printBusinessMenu()
 	{
 		printMenuLineSingle(15);
-		System.out.println("");
 		System.out.println("Business Menu");
 		printMenuLineSingle(15);
-		System.out.println("");
 		System.out.println("1. View Booking Timetable");
 		System.out.println("2. Add Employee");
-		System.out.println("3. Remove Employee");
-		System.out.println("4. Logout");
+		System.out.println("3. Logout");
+		System.out.print("\nPlease enter your choice: ");
 	}
 	
-	public void menuInput(Scanner userInput, ArrayList<Customer> customers, ArrayList<Business> businesses, ArrayList<Employee> employees) 
+	public void printAvailabilities(Scanner userInput, ArrayList<Customer> customers, 
+			ArrayList<Business> businesses, ArrayList<Employee> employees)
 	{
-		//create objects for functions
-		//Scanner keyInput = new Scanner(System.in);
-		Registration register = new Registration();
+		int menuInput = 0;
 		
-		Login login = new Login();
-		
-		int input;
-		int loginReturnValue = 0;
-		boolean cond = false;
-		
-		do {
-			cond = false;
-			menuDisplay();
-		try {
-			input = userInput.nextInt();
-		
-			switch (input) {
-			case 1: 
-				login.getUsernamePassword(userInput);
-				
-				loginReturnValue = login.login(customers, businesses);
-				
-				if(loginReturnValue == 0)
-				{
-					System.out.println("Login failed. Incorrect username or password.");
-				}
-				else if (loginReturnValue == 1)
-				{
-					System.out.println("Login successful. Printing customer menu.");
-					System.out.println("");
-					cond = true;
-					
-					printCustomerMenu();
-					
-					input = userInput.nextInt();
-					
-					if (input == 1)
-					{
-						//SIMULATING
-						int service;
-						
-						System.out.println("1. Workout");
-						System.out.println("2. Dance Class");
-						System.out.println("3. Consulation");
-						System.out.print("Enter service to view:");
-						service = userInput.nextInt();
-						ViewBusiAvail v = new ViewBusiAvail();
-						v.viewAvailabilities(customers, businesses, service);
-						//END OF SIM
-					}
-					else if (input == 2)
-					{
-						cond = false;
-						break;
-					}
-				}
-				else if (loginReturnValue == 2)
-				{
-					System.out.println("Login successful. Printing business menu.");
-					System.out.println("");
-					cond = true;
-					
-					printBusinessMenu();
-					
-					input = userInput.nextInt();
-					
-					if (input == 1)
-					{
-						//SIMULATING
-						int service;
-						
-						System.out.println("1. Workout");
-						System.out.println("2. Dance Class");
-						System.out.println("3. Consulation");
-						System.out.print("Enter service to view:");
-						service = userInput.nextInt();
-						ViewBusiAvail v = new ViewBusiAvail();
-						v.viewAvailabilities(customers, businesses, service);
-						//END OF SIM
-					}
-					else if (input == 2)
-					{
-						register.addNewEmployee(employees);
-					}
-					else if (input == 3)
-					{
-						System.out.println("Under construction.");
-						cond = false;
-						break;
-					}
-					else if (input == 4)
-					{
-						cond = false;
-						break;
-					}
-				}
+		while (menuInput != 1)
+		{
+			// viewing availabilities
+			
+			System.out.println("\n1. Exit");
+			System.out.print("\nPlease enter your choice: ");
+			
+			if (menuInput == 1)
+			{
 				break;
-			case 2:
-					register.getUserValues(customers, businesses);
-					
-					if (register.registerNewCust(customers, businesses)) {
-						//code for when register is successful(returns true)
-					} else {
-						//code for when register is unsuccessful(returns false)
-					}
-				break;
-			default:
-				System.out.println("Invalid input, please try again.");
-				break;
-			} 
-		} catch (InputMismatchException e) {
-			// display exception message when user inputs a string value
-			System.out.println("Please enter a numerical value.");
-			// clears buffer
-			userInput.nextLine();
-		}}
-	 	while (cond == false); 
+			}
+			else
+			{
+				System.out.println("\nInvalid user input. Please try again.");
+			}
+		}
 	}
 	
-	private void printMenuLineSingle(int numberOfLines)
+	public void printMenuLineSingle(int numberOfLines)
 	{
 		int counter = 0;
 		
@@ -170,9 +199,11 @@ public class Menu
 		{
 			System.out.print("-");
 		}
+		
+		System.out.println("");
 	}
 	
-	private void printMenuLineDouble(int numberOfLines)
+	public void printMenuLineDouble(int numberOfLines)
 	{
 		int counter = 0;
 		
@@ -180,6 +211,8 @@ public class Menu
 		{
 			System.out.print("=");
 		}
+		
+		System.out.println("");
 	}
 }
 
