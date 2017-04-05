e7571e4e798c83337a585954b91005bcacb2a187
Tony Mao <s3602027@student.rmit.edu.au>
2017-04-05 11:05:49 +1000

    ava

--- booking-system-sept-2017/src/test/ViewBusiAvailTest.java
+++ booking-system-sept-2017/src/test/ViewBusiAvailTest.java
@@ -16,6 +16,7 @@ public class ViewBusiAvailTest {
 	public void initServiceTableTest() {
 		int TABLE_WIDTH = 5;
 		int TABLE_HEIGHT = 8;
+		
 		String[][] serviceTable = new String[TABLE_HEIGHT][TABLE_WIDTH];
 		
 		assertEquals(null, serviceTable[0][0]);
@@ -27,7 +28,10 @@ public class ViewBusiAvailTest {
 				assertEquals("9:00-10:00", serviceTable[0][col]);
 				serviceTable[row][3] = "12:00-13:00";
 				assertEquals("12:00-13:00", serviceTable[row][3]);
+				
 			}
 		}
-	}	
+	}
+	/*@Test*/
+	
 }
