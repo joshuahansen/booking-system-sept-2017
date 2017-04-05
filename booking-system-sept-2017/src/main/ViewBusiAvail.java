b01c722c4cbd38c20929ef602d9f663e571cec21
Tony Mao <s3602027@student.rmit.edu.au>
2017-04-05 10:49:02 +1000

    Update ViewBusiAvail.java

--- booking-system-sept-2017/src/main/ViewBusiAvail.java
+++ booking-system-sept-2017/src/main/ViewBusiAvail.java
@@ -6,32 +6,28 @@ import users.Customer;
 
 public class ViewBusiAvail {
 	
-	private String[] services = {"WORKOUT", "YOGA CLASS", "CONSULATION"};
 	private String[] weekDay = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
 	private int TABLE_HEIGHT = 8;
 	private int TABLE_WIDTH = 5;
 	private String[][] serviceTime = new String[TABLE_HEIGHT][TABLE_WIDTH];
-    private String notAvailable = "XXXXXXXXXXX";
+    private String notAvailable = "UNAVAILABLE";
 	
     public ViewBusiAvail() {
 		
 	}
 	
-	public void viewAvailabilities(ArrayList<Customer> customers, ArrayList<Business> businesses, int service) {
+	public void viewAvailabilities(ArrayList<Customer> customers, ArrayList<Business> businesses) {
 		
-		service--;
 		//initializes the times for each weekday.
-		initServiceTable(service);
+		initServiceTable();
 		
 		//prints actual table to console
-		printTable(service);
+		printTable();
 		
 	}
 	
-	void printTable(int service) {
+	void printTable() {
 		
-		//updates user on which service times is being viewed.
-		System.out.println("VIEWING TIMES FOR" + services[service] + ":");
 		printBorder();
 		System.out.print("     ");
 		
@@ -61,7 +57,7 @@ public class ViewBusiAvail {
 		}
 	}
 	
-	void initServiceTable(int service) {
+	void initServiceTable() {
 		
 		//initialises the default values for all array elements
 		for (int col = 0; col < TABLE_WIDTH; col++) {
@@ -77,38 +73,15 @@ public class ViewBusiAvail {
 					
 				serviceTime[4][col] = "13:00-14:00";
 					
-		     	serviceTime[5][col] = "14:00-15:00";
+		     		serviceTime[5][col] = "14:00-15:00";
 					
 				serviceTime[6][col] = "15:00-16:00";
 					
 				serviceTime[7][col] = "16:00-17:00";
 					
-				//manually set array availabilities for notAvailable for each service.?
-				if (service == 0) {
-					serviceTime[row][0] = notAvailable;
-					serviceTime[2][col] = notAvailable;
-					serviceTime[3][3] = notAvailable;
-					serviceTime[2][4] = notAvailable;
-						
-				} else if (service == 1) {
-						
-					serviceTime[row][3] = notAvailable;
-					serviceTime[2][2] = notAvailable;
-					serviceTime[3][1] = notAvailable;						
-					serviceTime[0][2] = notAvailable;
-					serviceTime[6][4] = notAvailable;
-					
-				} else if (service == 2) {
-					serviceTime[4][col] = notAvailable;
-					serviceTime[3][3] = notAvailable;
-					serviceTime[1][3] = notAvailable;						
-					serviceTime[5][2] = notAvailable;
-					serviceTime[5][2] = notAvailable;
-					serviceTime[1][0] = notAvailable;
-						
-				} else {
-					System.out.println("invalid service");
-				}
+				serviceTime[row][2] = notAvailable;
+				
+				serviceTime[3][col] = notAvailable;
 			}
 		}
 	}
