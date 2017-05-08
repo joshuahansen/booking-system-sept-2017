ee76fbac8eb3a2d33287d3c324f11ab55353bb3f
rmit-s3601235-lohgan-nash <s3601235@student.rmit.edu.au>
2017-05-08 13:14:28 +1000

    Availabilities fixes and GUI
    Change-Id: Ieb63870bdc8f125ec92576949d471acc5a2f7917
    Signed-off-by: rmit-s3601235-lohgan-nash <s3601235@student.rmit.edu.au>

--- booking-system-sept-2017/src/javafx/EmployeeAvailiabilitiesController.java
+++ booking-system-sept-2017/src/javafx/EmployeeAvailiabilitiesController.java
@@ -13,6 +13,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
 import main.Session;
 import users.Business;
 import users.Employee;
@@ -20,7 +21,7 @@ import users.Employee;
 public class EmployeeAvailiabilitiesController implements Initializable{
 	
 	private ArrayList<Business> businesses;
-	public Button btn[][] = new Button[5][4];
+	public Button btn[][] = new Button[7][3];
 
 	private int busPos;
 	private int emplPos;
@@ -45,9 +46,11 @@ public class EmployeeAvailiabilitiesController implements Initializable{
 	@FXML private Button button18;
 	@FXML private Button button19;
 	@FXML private Button button20;
+	@FXML private Button button21;
 
 	@FXML private ComboBox<String> employeeCombo;
 	@FXML private MenuButton employeeDropdown;
+	@FXML private BorderPane empBorder;
     private final ObservableList<String> employeeList = FXCollections.observableArrayList();
 
 	private ArrayList<MenuItem> menuButtons;
@@ -65,23 +68,24 @@ public class EmployeeAvailiabilitiesController implements Initializable{
 		    btn[0][0] = button1;
 		    btn[0][1] = button2;
 		    btn[0][2] = button3;
-		    btn[0][3] = button4;
-		    btn[1][0] = button5;
-		    btn[1][1] = button6;
-		    btn[1][2] = button7;
-		    btn[1][3] = button8;
-		    btn[2][0] = button9;
-		    btn[2][1] = button10;
-		    btn[2][2] = button11;
-		    btn[2][3] = button12;
-		    btn[3][0] = button13;
-		    btn[3][1] = button14;
-		    btn[3][2] = button15;
-		    btn[3][3] = button16;
-		    btn[4][0] = button17;
-		    btn[4][1] = button18;
-		    btn[4][2] = button19;
-		    btn[4][3] = button20;
+		    btn[1][0] = button4;
+		    btn[1][1] = button5;
+		    btn[1][2] = button6;
+		    btn[2][0] = button7;
+		    btn[2][1] = button8;
+		    btn[2][2] = button9;
+		    btn[3][0] = button10;
+		    btn[3][1] = button11;
+		    btn[3][2] = button12;
+		    btn[4][0] = button13;
+		    btn[4][1] = button14;
+		    btn[4][2] = button15;
+		    btn[5][0] = button16;
+		    btn[5][1] = button17;
+		    btn[5][2] = button18;
+			btn[6][0] = button19;
+			btn[6][1] = button20;
+			btn[6][2] = button21;
 		}
 	 
 	 public void addEmployeeDropdown() 
@@ -98,6 +102,7 @@ public class EmployeeAvailiabilitiesController implements Initializable{
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) 
 	{
+		empBorder.setStyle("-fx-background-color: #F9F9F9;");
 		initializeButtonArray();
 		for(int empNo = 0; empNo < businesses.get(busPos).employees.size(); empNo++)
 		{
@@ -149,20 +154,17 @@ public class EmployeeAvailiabilitiesController implements Initializable{
 	        			empl.availableTimes[i][0] = 0;
 	        			empl.availableTimes[i][1] = 0;
 	        			empl.availableTimes[i][2] = 0;
+	        			empl.availableTimes[i][3] = 0;
 	        		}
 	        		else if(j == 1) 
 	        		{
-	        			empl.availableTimes[i][3] = 0;
 	        			empl.availableTimes[i][4] = 0;
+	        			empl.availableTimes[i][5] = 0;
+	        			empl.availableTimes[i][6] = 0;
 	        		}
 	        		else if(j == 2) 
 	        		{
-	        			empl.availableTimes[i][5] = 0;
-	        			empl.availableTimes[i][6] = 0;
 	        			empl.availableTimes[i][7] = 0;
-	        		}
-	        		else if (j == 3)
-	        		{
 	        			empl.availableTimes[i][8] = 0;
 	        			empl.availableTimes[i][9] = 0;
 	        		}
@@ -174,23 +176,20 @@ public class EmployeeAvailiabilitiesController implements Initializable{
 	        			empl.availableTimes[i][0] = 1;
 	        			empl.availableTimes[i][1] = 1;
 	        			empl.availableTimes[i][2] = 1;
+	        			empl.availableTimes[i][3] = 1;
 	        		}
 	        		else if(j == 1) 
 	        		{
-	        			empl.availableTimes[i][3] = 1;
 	        			empl.availableTimes[i][4] = 1;
+	        			empl.availableTimes[i][5] = 1;
+	        			empl.availableTimes[i][6] = 1;
 	        		}
 	        		else if(j == 2) 
 	        		{
-	        			empl.availableTimes[i][5] = 1;
-	        			empl.availableTimes[i][6] = 1;
 	        			empl.availableTimes[i][7] = 1;
-	        		}
-	        		else if (j == 3)
-	        		{
 	        			empl.availableTimes[i][8] = 1;
 	        			empl.availableTimes[i][9] = 1;
-	        		}	
+	        		}
 	        	}
 	        }
 		}
@@ -205,7 +204,7 @@ public class EmployeeAvailiabilitiesController implements Initializable{
 	        for(int j = 0; j <empl.availableTimes[i].length; j++) 
 	        {
 	            
-	        	if (j == 0 || j == 1 || j == 2) 
+	        	if (j == 0 || j == 1 || j == 2 || j == 3) 
 	        	{
 	        		if(empl.availableTimes[i][j] == 1)
 	        		{
@@ -216,7 +215,7 @@ public class EmployeeAvailiabilitiesController implements Initializable{
 	        			btn[i][0].setText("Not Availiable");
 	        		}
 	        	}
-	        	else if (j == 3 || j == 4)
+	        	else if (j == 4 || j == 5 || j == 6)
 	        	{
 	        		if(empl.availableTimes[i][j] == 1)
 	        		{
@@ -227,7 +226,7 @@ public class EmployeeAvailiabilitiesController implements Initializable{
 	        			btn[i][1].setText("Not Availiable");
 	        		}
 	        	}
-	        	else if (j == 5 || j == 6 || j == 7)
+	        	else if (j == 7 || j == 8 || j == 9)
 	        	{
 	        		if(empl.availableTimes[i][j] == 1)
 	        		{
@@ -237,19 +236,7 @@ public class EmployeeAvailiabilitiesController implements Initializable{
 	        		{
 	        			btn[i][2].setText("Not Availiable");
 	        		}
-	        	}
-	        	else if (j == 8 || j == 9)
-	        	{
-	        		if(empl.availableTimes[i][j] == 1)
-	        		{
-	        			btn[i][3].setText("Availiable");
-	        		}
-	        		else if(empl.availableTimes[i][j] == 0)
-	        		{
-	        			btn[i][3].setText("Not Availiable");
-	        		}
-	        	}
-	        	
+	        	}    	
 	        }
 	    }
 	}
